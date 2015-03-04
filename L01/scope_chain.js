d1679c8abf9885d05a36fab042efa9239750b57f
Marius Cristea <marius.cristea@irian.ro>
2015-03-04 15:52:49 +0200

    L01 - scope chain complete

--- L01/scope_chain.js
+++ L01/scope_chain.js
@@ -1,14 +1,33 @@
 function first() {
+    var a = 1;
+    var l = 'look';
+
+    console.log('first a: ' + a);
+    console.log('first l: ' + l);
+
     second();
 
     function second() {
+        var a = 2;
+
+        console.log('second a: ' + a);
+        console.log('second l: ' + l);
+
         third();
 
         function third() {
+            var a = 3;
+
+            console.log('third a: ' + a);
+            console.log('third l: ' + l);
+
             fourth();
 
             function fourth() {
-                // do something
+                var a = 2;
+
+                console.log('fourth a: ' + a);
+                console.log('fourth l: ' + l);
             }
         }
     }

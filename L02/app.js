611e9c1fc2a6f5f9730aedec7df62c9dbc624c25
Marius Cristea <marius.cristea@irian.ro>
2015-03-30 08:31:42 +0200

    L02 - Our First Controller

--- L02/app.js
+++ L02/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('store', []);
+(function() {
+    var app = angular.module('store', []);
+
+    var pizza = {
+        name: '4 STAGIONI',
+        price: 27.5,
+        weight: 550,
+        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare'
+    }
+})();

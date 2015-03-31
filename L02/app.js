25859316a895fedb8bd635b16e92f8174d3bf549
Marius Cristea <marius.cristea@irian.ro>
2015-03-31 22:26:54 +0200

    L03 - Using Arrays

--- L02/app.js
+++ L02/app.js
@@ -1,16 +1,19 @@
 (function() {
     var app = angular.module('pizzaStore', []);
 
-    var pizza = {
+    var pizzas = [{
         name: '4 STAGIONI',
         price: 27.5,
         weight: 550,
-        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare',
-        canPurchase: false,
-        soldOut: true
-    }
+        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare'
+    }, {
+        name: 'DIAVOLA',
+        price: 25,
+        weight: 450,
+        ingredients: 'sos rosii, mozzarella, salam picant, oregano'
+    }];
 
     app.controller('StoreController', function() {
-        this.product = pizza;
+        
     });
 })();

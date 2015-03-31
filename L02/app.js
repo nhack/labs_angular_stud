57344f65b366299c70fa4f9f83ab0d611bcfc854
Marius Cristea <marius.cristea@irian.ro>
2015-03-31 22:10:56 +0200

    L02 - canpurchase

--- L02/app.js
+++ L02/app.js
@@ -5,7 +5,9 @@
         name: '4 STAGIONI',
         price: 27.5,
         weight: 550,
-        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare'
+        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare',
+        canPurchase: false,
+        soldOut: true
     }
 
     app.controller('StoreController', function() {

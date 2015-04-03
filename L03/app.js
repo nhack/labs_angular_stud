8ef7e1d8e2f8c124a7ae76bfcb4296986ff02682
Marius Cristea <marius.cristea@irian.ro>
2015-04-03 18:08:05 +0300

    L03 - Using Filters Final

--- L03/app.js
+++ L03/app.js
@@ -5,12 +5,18 @@
         name: '4 STAGIONI',
         price: 27.5,
         weight: 550,
-        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare'
+        ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare',
+        images: [
+            'images/4_stagioni-01.jpg'
+        ]
     }, {
         name: 'DIAVOLA',
         price: 25,
         weight: 450,
-        ingredients: 'sos rosii, mozzarella, salam picant, oregano'
+        ingredients: 'sos rosii, mozzarella, salam picant, oregano',
+        images: [
+            'images/diavola-01.jpg'
+        ]
     }]
 
     app.controller('StoreController', function() {

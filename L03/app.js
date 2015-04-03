b2a0034942e107a82579dce1a2d681c51dad65b6
Marius Cristea <marius.cristea@irian.ro>
2015-04-03 18:41:23 +0300

    L03 - Display all images - final

--- L03/app.js
+++ L03/app.js
@@ -7,7 +7,9 @@
         weight: 550,
         ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare',
         images: [
-            'images/4_stagioni-01.jpg'
+            'images/4_stagioni-01.jpg',
+            'images/4_stagioni-02.jpg',
+            'images/4_stagioni-03.jpg'
         ]
     }, {
         name: 'DIAVOLA',
@@ -15,8 +17,16 @@
         weight: 450,
         ingredients: 'sos rosii, mozzarella, salam picant, oregano',
         images: [
-            'images/diavola-01.jpg'
+            'images/diavola-01.jpg',
+            'images/diavola-02.jpg',
+            'images/diavola-03.jpg'
         ]
+    }, {
+        name: 'MARGHERITA',
+        price: 17.5,
+        weight: 350,
+        ingredients: 'sos rosii, mozzarella, oregano',
+        images: []
     }]
 
     app.controller('StoreController', function() {

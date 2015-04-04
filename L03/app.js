71de5bf617444cd243de6f4434c6b7df0745fa93
Marius Cristea <marius.cristea@irian.ro>
2015-04-04 15:20:05 +0300

    L03 - Tabs

--- L03/app.js
+++ L03/app.js
@@ -10,7 +10,18 @@
             'images/4_stagioni-01.jpg',
             'images/4_stagioni-02.jpg',
             'images/4_stagioni-03.jpg'
-        ]
+        ],
+        reviews: [{
+            stars: 5,
+            body: "I love this pizza!",
+            author: "joe@example.org",
+            createdOn: 1397490980837
+        }, {
+            stars: 1,
+            body: "This pizza sucks.",
+            author: "tim@example.org",
+            createdOn: 1397490980837
+        }]
     }, {
         name: 'DIAVOLA',
         price: 25,
@@ -20,13 +31,40 @@
             'images/diavola-01.jpg',
             'images/diavola-02.jpg',
             'images/diavola-03.jpg'
-        ]
+        ],
+        reviews: [{
+            stars: 3,
+            body: "I think this pizza was just OK, could honestly be more spicy, IMO.",
+            author: "JimmyDean@example.org",
+            createdOn: 1397490980837
+        }, {
+            stars: 4,
+            body: "Any pizza with chilly is for me!",
+            author: "gemsRock@example.org",
+            createdOn: 1397490980837
+        }]
     }, {
         name: 'MARGHERITA',
         price: 17.5,
         weight: 350,
         ingredients: 'sos rosii, mozzarella, oregano',
-        images: []
+        images: [],
+        reviews: [{
+            stars: 1,
+            body: "This pizza is WAY too expensive for its value.",
+            author: "turtleguyy@example.org",
+            createdOn: 1397490980837
+        }, {
+            stars: 1,
+            body: "BBW: Simple Pizza != High Quality.",
+            author: "LouisW407@example.org",
+            createdOn: 1397490980837
+        }, {
+            stars: 1,
+            body: "Don't waste your $$$!",
+            author: "nat@example.org",
+            createdOn: 1397490980837
+        }]
     }]
 
     app.controller('StoreController', function() {

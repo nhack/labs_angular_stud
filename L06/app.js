caa5af458c8d5cb040c710deada78c3bfd7cead0
Marius Cristea <marius.cristea@irian.ro>
2015-04-28 16:16:39 +0300

    L06 - Http Service - final

--- L06/app.js
+++ L06/app.js
@@ -1,10 +1,13 @@
 (function() {
     var app = angular.module('pizzaStore', ['pizzaStore-products']);
 
-    app.controller('StoreController', function() {
+    app.controller('StoreController', ['$http', function($http) {
         var store = this;
         store.products = [];
-    });
+        $http.get('/store-products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
     app.controller('ReviewController', function() {
         this.review = {};

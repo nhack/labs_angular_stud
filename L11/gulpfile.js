aa32bb388abdafc8feb742aee4cb365a7d8e4b2f
Marius Cristea <marius.cristea@irian.ro>
2015-05-24 23:17:06 +0300

    L11 - Inject

--- L11/gulpfile.js
+++ L11/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var inject = require('gulp-inject');
 var mainBowerFiles = require('main-bower-files');
 
-gulp.task('index', function() {
+gulp.task('inject', function() {
     var target = gulp.src('./index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths: 
     var bower = gulp.src(mainBowerFiles(), {
